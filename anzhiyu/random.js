93e854afc3ba1ed9a98b3baefc396af671379f3f
natsurainko <a-275@qq.com>
2024-10-23 23:25:52 +0800

    Site updated: 2024-10-23 23:25:52

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/23/hello-world/"];function toRandomPost(){
+var posts=["2024/10/23/hello-world/","2024/10/23/使用-hexo-github-pages-搭建个人博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
