3887224d7a72f88db29916ed896781d40f1f2164
natsurainko <a-275@qq.com>
2025-03-17 22:51:33 +0800

    Site updated: 2025-03-17 22:51:32

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/27/Windows-使用-VMware-17-Pro-安装-Mac-OS，搭建-iOS-开发环境/","2024/10/23/hello-world/","2024/10/23/使用-hexo-github-pages-搭建个人博客/"];function toRandomPost(){
+var posts=["2025/03/17/ConnectX 服务端部署指南/","2024/10/27/Windows-使用-VMware-17-Pro-安装-Mac-OS，搭建-iOS-开发环境/","2024/10/23/hello-world/","2024/10/23/使用-hexo-github-pages-搭建个人博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
